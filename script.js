16a2a7c8012a7426ecf10cf3aaef3115ebc7a45e
AhmetEmreSengul <ahmetemresengul.34@gmail.com>
2025-10-28 18:14:22 +0300

    Adding animation loop and static rotation

--- script.js
+++ script.js
@@ -6,13 +6,28 @@ const SEESAW_LENGTH = 400;
 const SEESAW_X = canvas.width / 2;
 const SEESAW_Y = canvas.height / 2;
 
-c.fillStyle = "#8B4513";
-c.fillRect(SEESAW_X - SEESAW_LENGTH / 2, SEESAW_Y - 10, SEESAW_LENGTH, 20);
+let activeAngle = 15;
 
-c.fillStyle = "#333";
-c.beginPath();
-c.moveTo(SEESAW_X - 15, SEESAW_Y);
-c.lineTo(SEESAW_X + 15, SEESAW_Y);
-c.lineTo(SEESAW_X, SEESAW_Y + 15);
-c.closePath();
-c.fill();
+function draw() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  c.save();
+  c.translate(SEESAW_X, SEESAW_Y);
+  c.rotate((activeAngle * Math.PI) / 180);
+
+  c.fillStyle = "#8B4513";
+  c.fillRect(-SEESAW_LENGTH / 2, -10, SEESAW_LENGTH, 20);
+
+  c.restore();
+
+  c.fillStyle = "#333";
+  c.beginPath();
+  c.moveTo(SEESAW_X - 15, SEESAW_Y);
+  c.lineTo(SEESAW_X + 15, SEESAW_Y);
+  c.lineTo(SEESAW_X, SEESAW_Y + 15);
+  c.closePath();
+  c.fill();
+
+  requestAnimationFrame(draw);
+}
+draw();
